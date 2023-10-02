5ee806565799e08e8fc738e57bc43c115b9e8f17
LinkCFF <cfierro@unal.edu.co>
2023-10-01 23:07:40 -0500

    Se está replanteando el calendar

--- app/models/day.rb
+++
@@ -1,4 +0,0 @@
-class Day < ApplicationRecord
-    # has_many :events ,
-    # belongs_to :pet
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :crud, only: [:index, :create, :show, :update, :destroy]
       resources :pets, only: [:index, :create, :show, :update, :destroy]
       resources :events, only: [:index, :create, :show, :update, :destroy]
       resources :clients, only: [:index, :create, :show, :update, :destroy]
-      resources :days, only: [:index, :create, :show, :update, :destroy]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
